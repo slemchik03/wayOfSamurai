f6146292b5927c67de74d4c4442cffddeca9ce22
Kompik <32t34>
2021-05-22 23:27:32 +0300

    com

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className="header">
+            <img src={"https://upload.wikimedia.org/wikipedia/commons/a/ab/Apple-logo.png"} alt="logo" />
+        </header>
+    )
+}
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,34 @@
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img src="https://img-19.ccm2.net/8vUCl8TXZfwTt7zAOkBkuDRHiT8=/1240x/smart/b829396acc244fd484c5ddcdcb2b08f3/ccmcms-commentcamarche/20494859.jpg" alt="xdd" />
+            </div>
+
+            <div>
+                ava + description
+        </div>
+
+            <div>
+                my post
+
+          <div>
+                    new post
+          </div>
+
+                <div>
+                    <div>
+                        post 1
+            </div>
+
+                    <div>
+                        post 2
+            </div>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default Profile;
